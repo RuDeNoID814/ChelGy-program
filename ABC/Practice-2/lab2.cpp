105d26d60a93f577c29d093944b12b378c76c666
MARON <rudenko@rud-m.ru>
2026-05-08 04:56:51 +0500

    ABC practice 2 redacted

--- ABC/Practice-2/lab2.cpp
+++ ABC/Practice-2/lab2.cpp
@@ -1,4 +1,32 @@
 #include <iostream>
+#include <stdio.h>
 
 using namespace std;
 
+//:OUTPUT
+//:INPUT
+//:Clobbers
+//r - register
+//m - memory
+//p - address
+//= - write
+//+ - read and write
+
+int main()
+{
+    int a, b, c, d, e, x;
+    // Показываем вставку, по которой делать вычисление
+    cout << "x=(((a+b)*c)-d):e - подставь числа в переменные от 1 до 100, чтобы сложить их";
+    // Принимаем числа в переменные
+    cout << "a = "; cin >> a;
+    cout << "b = "; cin >> b;
+    cout << "c = "; cin >> c;
+    cout << "d = "; cin >> d;
+    cout << "e = "; cin >> e;
+
+    __asm__ ()
+
+
+    // Выводим x
+    cout << "x = " << x << endl;
+}

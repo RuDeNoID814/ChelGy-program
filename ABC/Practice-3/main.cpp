ea7f5c50dc1c3dc5d5a8e7a38eb0a57e08fd6cf0
MARON <rudenko@rud-m.ru>
2026-05-15 05:18:18 +0500

    ABC finish Practice-3

--- ABC/Practice-3/main.cpp
+++ ABC/Practice-3/main.cpp
@@ -1,40 +1,58 @@
 #include <iostream>
 
-//unsigned char a,b;
-//
 //Программа должна запросить значения переменных у пользователя. Написать ассемблерную вставку, которая вычислит a+b и запишет в две переменные типа bool наличие переполнения для знаковых/беззнаковых чисел.
 //Программа должна вывести на экран:
 //Результат сложения для чисел со знаком
 //Результат сложения для чисел без знака
 //Было ли переполнение для чисел со знаком
 //Было ли переполнение для чисел без знака
-//Пример:
-//a=255
-//b=255
-//a+b=-2 (signed)
-//a+b=254 (unsigned)
-//no overflow (signed)
-//overflow (unsigned)
-//Необходимо изучить команды: jc, jnc, jo,jno
 
 using namespace std;
 
-
-
 int main()
 {
-    unsigned char a, b, z;
-    bool notOverFlow, isOverFlow = 0;
+    unsigned char a, b, z; // создаем переменные. т.к char символный тип, то далее будем вводить в int, а потом переводить в char
+    bool isSignedOverflow = false; // bool для true/false на флагах of
+    bool isUnsignedOverflow = false; // true/false на флагах cf
 
 
     cout << "Эта программа позволяет вычислить наличие переполнения для знаковых/беззнаковых чисел. \n\n";
-    cout << "Введите переменные a и b: \n\n";
-    cout << "Переменная a: "; cin >> a;
-    cout << "Переменная b: "; cin >> b;
+    cout << "Введите переменные a и b, чтобы сложить их: \n\n";
+    int tmp; // создам временную переменную для чисда, потому что unsigned char считается как символ
+    cout << "Переменная a: "; cin >> tmp; a = (unsigned char)tmp; // вводим число в int, а далее переводим его в unsigned char
+    cout << "Переменная b: "; cin >> tmp; b = (unsigned char)tmp;
+    cout << "\n\n";
+
+    __asm__ (
+        "mov %3, %%al \n\t" // перемещаем a в младший регистр
+        "add %4, %%al \n\t" // прибавляем b + a
+        "mov %%al, %0 \n\t" // выводим al в z
+        "jae uns_cf \n\t" // если cf = 0, то прыгаем дальше
+        "movb $1, %2 \n\t" // если cf не 0, то присваиваем true(1) и итог cf = 1 - переполнен
+        "uns_cf: \n\t" // метка для прыжка
+        "jno sig_of \n\t" // если of = 0, то делаем прыжок
+        "movb $1, %1 \n\t" // если of не 0, то присваиваем true(1) и итог of =1 - переполнен
+        "sig_of: \n\t" // метка для прыжка
+        :"=m"(z), "=m"(isSignedOverflow), "=m"(isUnsignedOverflow) //OUTPUT
+        :"m"(a), "m"(b) //INPUT
+        :"%eax" //Clobbers
+        );
 
 
+    // Блок для вывода знаковых signed
+    if(isSignedOverflow)
+        cout << "Знаковый(signed) = " << (int)(signed char)z << " - переполнение произошло \n";
+    else
+        cout << "Знаковый(signed) = " << (int)(signed char)z << " - переполнения не произошло \n";
 
-}
+
+    //Блок для вывода беззнаковых unsigned
+    if(isUnsignedOverflow)
+        cout << "Беззнаковый(unsigned) = " << (int)z << " - переполнение произошло \n";
+    else
+        cout << "Беззнаковый(unsigned) = " << (int)z << " - переполнения не произошло \n";
+
+    }
 
 
 //#include <stdio.h>
@@ -54,8 +72,8 @@ int main()
 //    "mov %2,%%al \n\t"
 //    "add %3,%%al \n\t"
 //    "mov %%al,%1 \n\t"
-//    "jnc isof \n\t"
-//    "movb $1,%0 \n\t"
+//    "jnc isof \n\t"    ЕСЛИ нетпереполнения то 0 и переходим к isof
+//    "movb $1,%0 \n\t"      ЕСЛИ переполнение то 1
 //    "isof: \n\t"
 //    :"=m"(isOverFlow),"=m"(z):"m"(a),"m"(b):"%eax","memory","cc");
 //    printf("%d\n",*(signed char*)(&z));

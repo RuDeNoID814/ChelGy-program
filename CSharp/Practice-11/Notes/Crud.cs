65365f984cb02e7eb79463eb725e36f1ab6a3c5b
Maksim <rudenko@rud-m.ru>
2026-05-25 21:23:33 +0500

    ORM completed from practice-11

--- CSharp/Practice-11/Notes/Crud.cs
+++ CSharp/Practice-11/Notes/Crud.cs
@@ -4,6 +4,8 @@ namespace Notes;
 
 public class Crud
 {
+    
+    // создает новый note и сохраняет его в БД.
     public static async Task<Note> Create(string text, DateTimeOffset createdAt, CancellationToken ct = default)
     {
         await using var db = new NotesContext();
@@ -17,6 +19,37 @@ public class Crud
         return note;
     }
     
+    // READ
+    public static async Task<List<Note>> Read(string search, CancellationToken ct = default)
+    {
+        await using var db = new NotesContext();
+        var result = await db.Notes
+            .Where(x => EF.Functions.Like(x.Text, $"%{search}%"))
+            .ToListAsync(ct);
+        return result;
+    }
     
+    // Поиск по id
+    public static async Task<Note?> Read(int id, CancellationToken ct = default)
+    {
+        await using var db = new NotesContext();
+        return await db.Notes.FirstOrDefaultAsync(x => x.Id == id, ct);
+    }
+
+    // Обновляет текст заметки
+    public static async Task Update(Note note, string text, CancellationToken ct = default)
+    {
+        await using var db = new NotesContext();
+        note.Text = text;
+        db.Notes.Update(note);
+        await db.SaveChangesAsync(ct);
+    }
     
+    // Delete
+    public static async Task Delete(Note note, CancellationToken ct = default)
+    {
+        await using var db = new NotesContext();
+        db.Notes.Remove(note);
+        await db.SaveChangesAsync(ct);
+    }
 }

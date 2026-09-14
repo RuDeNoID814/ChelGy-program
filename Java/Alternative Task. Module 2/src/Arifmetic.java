dac050896de1a2ea9ea1664411368ed5b56f0187
Maksim <rudenko@rud-m.ru>
2026-09-14 22:03:54 +0500

    alternative task. module 2

--- Java/Alternative Task. Module 2/src/Arifmetic.java
+++ Java/Alternative Task. Module 2/src/Arifmetic.java
@@ -5,7 +5,21 @@ public class Arifmetic {
         int[] numbers = {9, 8, 7, 6, 5, 4, 3, 2};
 
         for (char op : operations) {
-            System.out.println("9" + op + "8");
+            for(char op1 : operations) {
+                for(char op2 : operations) {
+                    for(char op3 : operations) {
+                        for(char op4 : operations) {
+                            for(char op5 : operations) {
+                                for(char op6 : operations) {
+                                    String numbTemp = ("9" + op + "8" + op1 + "7" + op2 + "6" + op3 + "5" + op4 + "4" + op5 + "3" + op6 +
+                                            "2").replace(" ","");
+                                    System.out.println(numbTemp);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
 
     }

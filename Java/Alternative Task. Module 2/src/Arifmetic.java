1895f5d39ef1779d0099ae0f80aad48dbe2eb96d
Maksim <rudenko@rud-m.ru>
2026-09-14 21:27:59 +0500

    added arifmetic lab 2

---
+++ Java/Alternative Task. Module 2/src/Arifmetic.java
@@ -0,0 +1,12 @@
+public class Arifmetic {
+    public static void main(String[] args) {
+
+        char[] operations = {'+', '-', '*', '/', ' '};
+        int[] numbers = {9, 8, 7, 6, 5, 4, 3, 2};
+
+        for (char op : operations) {
+            System.out.println("9" + op + "8");
+        }
+
+    }
+}
